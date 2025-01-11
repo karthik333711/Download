5a3828b8d71851c698cd9fc3eaab88e4e8dc34fa
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-01-11 13:26:35 +0530

    Update Remote One Pc.ps1

--- Remote One Pc.ps1
+++ Remote One Pc.ps1
@@ -8,6 +8,4 @@ cmdkey /add:$ComputerName /user:$Username /pass:$Password
 # Launch RDP
 Start-Process "mstsc.exe" -ArgumentList "/v:$ComputerName"
 
-# Optional: Remove stored credentials after use
-Start-Sleep -Seconds 5
-cmdkey /delete:$ComputerName
+
